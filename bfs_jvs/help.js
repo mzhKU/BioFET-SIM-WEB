ed4f8531504d13522fd2a53a7d96f5743d05c482
mzh <ma.hed@bluewin.ch>
2012-07-17 17:12:42 +0200

    Switched unit to cm^{-3}

--- bfs_jvs/help.js
+++ bfs_jvs/help.js
@@ -18,25 +18,36 @@ $(document).ready(function()
     var electron_mass   =           9.10938215e-31;
     var eps0            =           8.854187817e-12;
 
+    // Comes in as nm
     function get_np0(ltf)
     {
         var meff               = $('#meff').val()*electron_mass;
         var eps1               = $('#eps1').val()*eps0; 
+
         // Convert l_TF from nm to meters then square
         var lTFInMetersSquared = Math.pow(ltf*1E-9, 2);
+
+        // Convert m^{-3} to cm^{-3}
         var numerator          = hbarSquared*eps1*piPower4over3;
         var denominator        = meff*elChargeSquared*lTFInMetersSquared;
-        var np0                = Math.pow( numerator/denominator, 3).toExponential(3);
+        var np0                = (Math.pow( numerator/denominator, 3)*1E-6).toExponential(3)
         $('#np0').val(np0);
     }
 
+    // Comes in as centimeter qube
     function get_ltf(np0)
     {
         var meff        = $('#meff').val()*electron_mass;
         var eps1        = $('#eps1').val()*eps0; 
         var numerator   = hbarSquared*eps1*piPower4over3;
-        var denominator = meff*elChargeSquared*Math.pow(np0, 1.0/3.0);
-        // Convert back to nm and display two decimal digits
+
+        // m^{-3}
+        //var denominator = meff*elChargeSquared*Math.pow(np0, 1.0/3.0);
+
+        // Converting from cm^{-3} to m^{-3}
+        var denominator = meff*elChargeSquared*Math.pow(np0*1E6, 1.0/3.0);
+
+        // Convert from meter to nm and display two decimal digits
         var lTF         = roundit(Math.sqrt(numerator/denominator)*1E9, 2).toPrecision(3);
         $('#ltf').val(lTF);
     }
@@ -52,4 +63,18 @@ $(document).ready(function()
         var np0 = $(this).val();
         get_ltf(np0);
     }); // end np0 keyup
+
+    $('#meff').keyup(function()
+    {
+        var meff = $(this).val();
+        var ltf  = $('#ltf').val();
+        get_np0(ltf);
+    }); // end meff keyup
+
+    $('#eps1').keyup(function()
+    {
+        var eps1 = $(this).val();
+        var ltf = $('#ltf').val();
+        get_np0(ltf);
+    }); // end eps1 keyup
 }); // end ready
